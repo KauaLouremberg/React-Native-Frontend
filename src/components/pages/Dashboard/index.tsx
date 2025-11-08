5d1a41881b4cf34b050e0baf4356bae6ab47e01b
Louremberg <kaua.lima@layoutsistemas.com.br>
2025-11-08 12:36:25 -0300

    Dashboard vazia e api

--- src/components/pages/Dashboard/index.tsx
+++ src/components/pages/Dashboard/index.tsx
@@ -1,33 +1,8 @@
-import { Button } from "react-native";
-import api from "../../conexao/api";
+import { Texto } from "../../texto";
 
 export default function Dashboard() {
 
-//   async function criarUsuario() {
-//   try {
-//     const response = await api.post('/autenticacao/', {
-//       nome: 'kaua',
-//       email: 'kaualouremberg@gmail.com',
-//     });
-//     console.log(response.data);
-//   } catch (err: any) {
-//     console.error(err.response?.data || err.message);
-//   }
-// }
-
-const criarUsuario = async() => {
-  try {
-    const response = await api.post('/autenticacao/', {
-      nome: 'kaua',
-      email: 'kaualouremberg@gmail.com',
-    });
-    console.log(response.data)
-  } catch (err: any) {
-    console.error(err.reponse?.data)
-  }
-}
-
   return <>
-    <Button title="Teste Api" onPress={() => criarUsuario()} />
+    <Texto>Dashboard</Texto>
   </>;
 }
--- src/components/pages/login.tsx
+++ src/components/pages/login.tsx
@@ -4,7 +4,7 @@ import { colors } from '../../core/constants/colors';
 import { loginStyle } from '../../styles/login/login-style';
 import { Texto } from '../texto';
 
-export default function Login() {
+export default function Login({ navigation }: any) {
   const { section, wrapper, container, text, title } = loginStyle;
 
   return (
@@ -14,7 +14,6 @@ export default function Login() {
           <Texto style={text}>Bem-vindo ao Amparo.</Texto>
           <InfoIcon color={colors.neutral[500]} size={16} />
         </View>
-
         <Texto style={title}>Acesse sua conta aqui.</Texto>
       </View>
     </View>
