60520f57c3499c6ee98f09de67aff728af8ac440
Tiago Ramon <tiagoramon.sousa12@gmail.com>
2025-11-08 10:14:09 -0300

    adicionando texto e estilos iniciais

--- src/components/pages/login.tsx
+++ src/components/pages/login.tsx
@@ -1,5 +1,15 @@
+import { View } from 'react-native';
+import { loginStyle } from '../../styles/login/login-style';
 import { Texto } from '../texto';
 
 export default function Login() {
-  return <Texto>Login</Texto>;
+  const { section, text } = loginStyle;
+
+  return (
+    <View id="login-section" style={section}>
+      <View>
+        <Texto style={text}>Bem-vindo ao Amparo.</Texto>
+      </View>
+    </View>
+  );
 }
---
+++ src/styles/login/login-style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '../../core/constants/colors';
+import { POPPINS } from '../../core/constants/poppins';
+
+export const loginStyle = StyleSheet.create({
+  section: {
+    paddingTop: 40,
+    paddingHorizontal: 24,
+  },
+  text: {
+    color: colors.neutral[500],
+    fontFamily: POPPINS.medium,
+  },
+});
