9164a7daa98ae02358ba89a9fbace6161758ec4c
Tiago Ramon <tiagoramon.sousa12@gmail.com>
2025-11-08 10:38:53 -0300

    estilizando container do texto e icone

--- src/components/pages/login.tsx
+++ src/components/pages/login.tsx
@@ -1,14 +1,17 @@
+import { InfoIcon } from 'lucide-react-native';
 import { View } from 'react-native';
+import { colors } from '../../core/constants/colors';
 import { loginStyle } from '../../styles/login/login-style';
 import { Texto } from '../texto';
 
 export default function Login() {
-  const { section, text } = loginStyle;
+  const { section, container, text } = loginStyle;
 
   return (
     <View id="login-section" style={section}>
-      <View>
+      <View style={container}>
         <Texto style={text}>Bem-vindo ao Amparo.</Texto>
+        <InfoIcon color={colors.neutral[500]} size={16} />
       </View>
     </View>
   );
--- src/styles/login/login-style.ts
+++ src/styles/login/login-style.ts
@@ -7,6 +7,12 @@ export const loginStyle = StyleSheet.create({
     paddingTop: 40,
     paddingHorizontal: 24,
   },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
   text: {
     color: colors.neutral[500],
     fontFamily: POPPINS.medium,
