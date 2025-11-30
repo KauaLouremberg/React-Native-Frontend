2386150cec91b896da9fb6bbac688eb46baa31b3
Louremberg <kaua.lima@layoutsistemas.com.br>
2025-11-30 12:04:08 -0300

    MapMarker

--- src/store/userTypeSlice.tsx
+++ src/store/userTypeSlice.tsx
@@ -2,7 +2,9 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   responsavel_id: null,
-  amparado_id: null
+  responsavel_name: null,
+  amparado_id: null,
+  amparado_name: null
 };
 
 const userTypeSlice = createSlice({
@@ -10,13 +12,17 @@ const userTypeSlice = createSlice({
   initialState,
   reducers: {
     setUserType: (state, action) => {
-      const { responsavel_id, amparado_id } = action.payload;
+      const { responsavel_id, amparado_id, responsavel_name, amparado_name } = action.payload;
       state.responsavel_id = responsavel_id
+      state.responsavel_name = responsavel_name;
       state.amparado_id = amparado_id
+      state.amparado_name = amparado_name;
     },
     clearUserType: (state) => {
       state.responsavel_id = null;
+      state.responsavel_name = null;
       state.amparado_id = null;
+      state.amparado_name = null;
     },
   },
 });
